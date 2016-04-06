6b952d10c3940e6a364b405dd367c53e9a1e1be8
Bruce Burgess <bruceburgess67@yahoo.com>
2016-04-06 15:22:21 +0700

    added some test data and populate the collection

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,29 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Bruce Burgess on 4/6/16.
+//  Copyright © 2016 Red Raven Computing Studios. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
