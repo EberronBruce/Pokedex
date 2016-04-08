6ce94e38369ea8e6f92ea5713873c8df9e6c604e
Bruce Burgess <bruceburgess67@yahoo.com>
2016-04-08 22:20:08 +0700

    Some finishing touches in the PokeDex app. Finish the basics

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -22,19 +22,44 @@ class PokemonDetailVC: UIViewController {
     @IBOutlet weak var currentEvoImg: UIImageView!
     @IBOutlet weak var nextEvoImg: UIImageView!
     @IBOutlet weak var evoLabel: UILabel!
-    
-    
-    
+       
     var pokemon: Pokemon!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         nameLabel.text = pokemon.name
-        mainImage.image = UIImage(named: "\(pokemon.pokedexId)")
+        let img = UIImage(named: "\(pokemon.pokedexId)")
+        mainImage.image = img
+        currentEvoImg.image = img
         
         pokemon.downloadPokemonDetails { 
             //This will be called after download is done
+            self.updateUI()
         }
+    }
+    
+    func updateUI() {
+        descriptionLabel.text = pokemon.description
+        typeLabel.text = pokemon.type
+        defenseLabel.text = pokemon.defense
+        heightLabel.text = pokemon.height
+        weightLabel.text = pokemon.weight
+        pokedexLabel.text = "\(pokemon.pokedexId)"
+        baseAttackLabel.text = pokemon.attack
+        
+        if pokemon.nextEvolutionId == "" {
+            evoLabel.text = "No Evolutions"
+            nextEvoImg.hidden = true
+        } else {
+            nextEvoImg.hidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+            var str =  "Next Evolution: \(pokemon.nextEvolutionTxt)"
+            
+            if pokemon.nextEvolutionLvl != "" {
+                str += " - LVL \(pokemon.nextEvolutionLvl)"
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
